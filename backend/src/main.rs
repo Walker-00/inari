84262e72b9c797f59292edaa0c365ce663d07f28
Linus Walker <rissk_it@proton.me>
2025-03-10 08:58:13 +0630

    chore: desktop init

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| App::new())
+        .bind("127.0.0.1:9690")?
+        .run()
+        .await
 }
