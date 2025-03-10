a02b25add4d64972661ce16cbd8bfc269deb78b9
Linus Walker <rissk_it@proton.me>
2025-03-10 10:02:39 +0630

    chore: desktop init

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,15 @@
 use std::io;
 
 use actix_web::{App, HttpServer};
+use prost::Message;
+
+#[derive(Clone, PartialEq, Message)]
+pub struct Idk {
+    #[prost(string)]
+    pub name: String,
+    #[prost(uint32)]
+    pub age: u32,
+}
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
