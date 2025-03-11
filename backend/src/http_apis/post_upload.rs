ac8ae2c5e0576bb32aa641d56664b2d63669ff0e
Linus Walker <rissk_it@proton.me>
2025-03-11 20:17:58 +0630

    chore: post upload

--- backend/src/http_apis/post_upload.rs
+++ backend/src/http_apis/post_upload.rs
@@ -1,6 +1,22 @@
-use actix_protobuf::ProtoBuf;
-use surrealdb::Datetime;
+use actix_protobuf::{ProtoBuf, ProtoBufResponseBuilder};
+use actix_web::{HttpResponse, Responder};
 
-use crate::structures::{post::PostUp, static_vars::DB};
+use crate::structures::{
+    post::{PostDb, PostUp},
+    static_vars::DB,
+};
 
-pub fn post_upload(post: ProtoBuf<PostUp>) {}
+pub async fn post_upload(post: ProtoBuf<PostUp>) -> impl Responder {
+    let postdb: PostDb = match post.0.try_into() {
+        Ok(postdb) => postdb,
+        Err(shits) => {
+            return HttpResponse::BadRequest().protobuf(shits.to_string());
+        }
+    };
+    match DB.create::<Option<PostDb>>("rices").content(postdb).await {
+        Ok(Some(_)) => HttpResponse::Ok().protobuf("ok".to_string()),
+        Ok(None) => HttpResponse::InternalServerError()
+            .protobuf("shits just happened in creating a post".to_string()),
+        Err(shits) => HttpResponse::InternalServerError().protobuf(shits.to_string()),
+    }
+}
